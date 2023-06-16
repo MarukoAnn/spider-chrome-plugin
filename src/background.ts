9a72e500c91830c8b9403aa39c711940b5749399
zhangguian <zhangguian@zuzuche.com>
2023-06-16 18:54:28 +0800

    fix: 使用v2清单做网络拦截

--- src/background.ts
+++ src/background.ts
@@ -5,11 +5,15 @@ class ServerWorker {
 
     init(){
     console.log('234222')
-    console.log('chrome.declarativeNetRequest', chrome.declarativeNetRequest);
-    chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
-        const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
-        console.log(msg);
-    });
+    chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+        console.log("拦截到请求:", details);
+    }, { urls: ["https://m.ctrip.com/restapi/soa2/18631/queryProduct*","https://m.ctrip.com/restapi/soa2/18631/queryVehicleDetailList*"] },
+    ["extraHeaders", "requestHeaders"])
+    // console.log('chrome.declarativeNetRequest', chrome.declarativeNetRequest);
+    // chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
+    //     const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
+    //     console.log(msg);
+    // });
         // chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((res) => {
         //     console.log(res, 'res');
         // })
